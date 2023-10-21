1f66ee175aa9b28cddb69b11139b147aa363b39b
TommasoSviluppatore <tubia.st.tommaso@maxplanck.edu.it>
2023-10-21 15:01:15 +0200

    aaa
    aaaa

--- src/Principale/Main.java
+++
@@ -1,8 +0,0 @@
-package Principale;
-
-public class Main {
-	public static void main (String args[]) {
-		
-	}
-
-}
---
+++ src/gestioneChiamateLocalhost2/TCPBasic_Server.java
@@ -0,0 +1,67 @@
+package gestioneChiamateLocalhost2;
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
+
+public class TCPBasic_Server {
+	
+	private ServerSocket server;
+	private Socket connection;
+	private BufferedReader input;
+	private BufferedWriter output;
+	
+	public int numeroContatto=0;
+	
+	public TCPBasic_Server(){
+		
+		try {
+		
+		System.out.println("SERVER ATTIVO");
+        server = new ServerSocket(8081);
+        
+        System.out.println("SERVER IN ATTESA CHE UN CLIENT SI CONNETTA.....");
+        connection = server.accept();
+
+        System.out.println(connection);
+
+        input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        output = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        
+
+        while (true) {
+        	
+        	//INIZIO PROGRAMMA
+            String letta="" ;//= input.readLine();
+
+            if (letta.compareTo("EXIT") == 0) {
+                output.write("CLOSE");
+                output.flush();
+                System.out.println("Chiusura del canale");
+                break;
+            }
+
+            output.write(letta.toUpperCase() + "\n");
+            output.flush();
+            
+            
+            
+        }
+
+        input.close();
+        output.close();
+
+        connection.close();
+
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		TCPBasic_Server server=new TCPBasic_Server();
+
+	}
+}
