e26c6f2215c2c328a176372360ed76e815f56931
MatteoVignola <145805518+MatteoVignola@users.noreply.github.com>
2023-11-04 00:03:04 +0100

    Add files via upload

---
+++ sasso_carta_forbice-master/src/model/Server.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.io.*; 
+import java.net.*; 
+import java.util.*;
+
+public class Server {
+	private ServerSocket server;
+	private Socket connessione;
+	private BufferedReader dalClient;
+	private PrintStream alClient; // Scrive bytes mentre PrintWriter scrive caratteri
+	
+	public Server() { // costruttore
+		try {
+			// port (20000): se = 0 sceglie la prima porta libera
+			// backlog (5): lunghezza massima della coda delle richieste di connessione
+			server = new ServerSocket(20000, 5); // DatagramSocket per connessioni UDP
+			System.out.println("Server attivo");
+			System.out.println(server.getInetAddress());
+			connessione = server.accept();
+			dalClient = new BufferedReader(new InputStreamReader(connessione.getInputStream()));
+			alClient = new PrintStream(connessione.getOutputStream());
+		} catch (IOException e) {e.printStackTrace();}
+	}
+	
+	public void conversazione() {
+		String messaggio = "";
+		BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			alClient.println("Salve");
+			while(!messaggio.equals("fine")) {
+				
+				
+				
+					
+					
+				}
+			}
+			connessione.close();
+		} catch(IOException e) {e.printStackTrace();}
+	}
+}
