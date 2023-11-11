98bbb9531e5ee5ac972173efacb54b4b847beeb7
TommasoSviluppatore <tubia.st.tommaso@maxplanck.edu.it>
2023-11-11 10:35:59 +0100

    fhhffh
    gfhggf

--- sasso_carta_forbice-RamoMatteo/src/model/MainServer.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class MainServer {
-	
-	public static void main(String[] args) {
-		Server server = new Server();
-		server.conversazione();
-	}
-}
--- sasso_carta_forbice-master/src/model/MainClient.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class MainClient{
-	public static void main(String[] args) {
-		Client client = new Client();
-		client.conversazione();
-	}
-
-}
